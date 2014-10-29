13e567ecd6b3a0db5fbd34a94759453db142069a
Diego Alonso Uribe Gamez <diego.uribe.gamez@gmail.com>
2014-10-28 19:47:53 -0500

    update

--- server.js
+++ server.js
@@ -7,22 +7,6 @@ expressServer.get('/article/save/', function(req,res,next){
     res.send('Hello from article save');
 });
 
-expressServer.get('/article/remove/', function(req,res,next){
-    res.send('Hello from article remove');
-});
-
-expressServer.get('/article/add/', function(req,res,next){
-    res.send('Hello from article add');
-});
-
-expressServer.get('/article/see/:data', function(req,res,next){
-    res.send('Hello from article see');
-});
-
-expressServer.get('/article/edit/:data', function(req,res,next){
-    res.send('Hello from article edit');
-});
-
 expressServer.get('/article/list/', function(req,res,next){
     res.send('Hello from article list');
 });
