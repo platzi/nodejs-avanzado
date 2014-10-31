dd02cfd54d1e272c750f94f4ebe7fbb4db5df5ac
Diego Alonso Uribe Gamez <diego.uribe.gamez@gmail.com>
2014-10-31 17:47:15 -0500

    [update]

--- app/socketIO.js
+++ app/socketIO.js
@@ -1,15 +1,26 @@
-var Io = require('socket.io');
+var Io = require('socket.io'),
+	app_user = {};
 
 var SocketIO = function(config){
 	config = config || {};
 	var io = Io.listen(config.server);
 
 	io.sockets.on('connection', function(socket){
+		socket.join('some::room');
+
 		socket.emit('mejorandola', {hola:'soy mejorandola'});
 
 		socket.on('mejorandolo', function(data){
 			console.log(data);
 		});
+
+		socket.on('app_user',function(user){
+			app_user[user.id] = user
+		});
+
+		socket.on('disconnect', function(){
+			delete app_user[socket.store.id];
+		})
 	});
 }
 
